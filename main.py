19f2dd19381d9c6fe3e5fa39bf76b8f0138bd31e
Jelmer van Lune <jelmervanlune@gmail.com>
2022-06-29 13:04:25 +0200

    added argument parser

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from avalon import *
+import argparse
 
 if __name__ == "__main__":
-  num_games = 100
-  merlin = True
-  higher_order_evil = False
-  evil_agents_assasinate = True
+  parser = argparse.ArgumentParser(description="Run model of Avalon game.")
+  parser.add_argument("-n", "--num_games", type=int, default=100, help="Number of simulated games that will be played to average the results.")
+  parser.add_argument("-mer", "--merlin", type=bool, default=False, help="Specify whether to use a simplified version of Merlin as one of the good agents or not.")
+  parser.add_argument("-hoe", "--higher_order_evil", type=bool, default=True, help="Specify whether evil agents use higher order knowledge or not.")
+  parser.add_argument("-eaa", "--evil_agents_assasinate", type=bool, default=False, help="Specify whether evil agents can try to assasinate at the end of the game and win, this is done with a 1/3 chance.")
+  args = parser.parse_args()
+
+  num_games = args.num_games
+  merlin = args.merlin
+  higher_order_evil = args.higher_order_evil
+  evil_agents_assasinate =  args.evil_agents_assasinate
+
   num_good_wins = 0
   num_evil_wins = 0
   round_lengths = []

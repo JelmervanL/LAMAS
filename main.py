97a1d37aa7260e908648f1812f17850632eec6f9
Jelmer van Lune <jelmervanlune@gmail.com>
2022-06-29 15:41:25 +0200

    added argparser

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 from avalon import *
 import argparse
 
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description="Run model of Avalon game.")
   parser.add_argument("-n", "--num_games", type=int, default=100, help="Number of simulated games that will be played to average the results.")
-  parser.add_argument("-mer", "--merlin", type=bool, default=False, help="Specify whether to use a simplified version of Merlin as one of the good agents or not.")
-  parser.add_argument("-hoe", "--higher_order_evil", type=bool, default=True, help="Specify whether evil agents use higher order knowledge or not.")
-  parser.add_argument("-eaa", "--evil_agents_assasinate", type=bool, default=False, help="Specify whether evil agents can try to assasinate at the end of the game and win, this is done with a 1/3 chance.")
+  parser.add_argument("-mer", "--merlin", type=str2bool, nargs='?', const=True, default=False, help="Specify whether to use a simplified version of Merlin as one of the good agents or not.")
+  parser.add_argument("-hoe", "--higher_order_evil", type=str2bool, nargs='?', const=True, default=False, help="Specify whether evil agents use higher order knowledge or not.")
+  parser.add_argument("-eaa", "--evil_agents_assasinate", type=str2bool, nargs='?', const=True, default=False, help="Specify whether evil agents can try to assasinate at the end of the game and win, this is done with a 1/3 chance.")
   args = parser.parse_args()
 
   num_games = args.num_games
   merlin = args.merlin
   higher_order_evil = args.higher_order_evil
+  print(higher_order_evil)
   evil_agents_assasinate =  args.evil_agents_assasinate
-
-  num_games = 100
-  merlin = False
-  higher_order_evil = True
-  evil_agents_assasinate =  False
-
-  # print(merlin)
-  # print(higher_order_evil)
-  # print(evil_agents_assasinate)
 
   num_good_wins = 0
   num_evil_wins = 0
